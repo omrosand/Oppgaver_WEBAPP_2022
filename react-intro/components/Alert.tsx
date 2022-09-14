282036f2c2b943fd59f6bac52dd7d51c151597a1
Ole M. Røsand <77734811+omrosand@users.noreply.github.com>
2022-09-14 21:11:14 +0200

    btn styling

--- react-intro/components/Alert.tsx
+++ react-intro/components/Alert.tsx
@@ -1,18 +1,13 @@
-export default function Alert({
-  handleClickProp,
-  handleChangeProp,
-  inputValue,
-}: any) {
+export default function Alert({ handleClickProp, handleChangeProp }: any) {
   return (
     <>
       <form>
         <input
           type="text"
           placeholder="Type here.."
-          value={inputValue}
           onChange={handleChangeProp}
         />
-        <button type="submit" onClick={handleClickProp}>
+        <button type="submit" className="btn" onClick={handleClickProp}>
           I&apos;m a button!
         </button>
       </form>
